ef3b2946150e69ab1e5e6d7ae19ea4aa8e255c37
EstherMattos <silvaesthermattos@hotmail.com>
2022-06-18 11:08:18 -0300

    error no primeiro link

--- src/main/java/com/example/helloworld/HelloServlet.java
+++ src/main/java/com/example/helloworld/HelloServlet.java
@@ -2,39 +2,20 @@ package com.example.helloworld;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello-world")
+@WebServlet("/helloWorld")
 public class HelloServlet extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HelloServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HelloServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String message = "";
 
         String lang = request.getParameter("lang");
-        if(lang==null)
+        if(lang == null)
             lang = "pt";
         switch(lang){
             case "pt":
@@ -50,7 +31,7 @@ public class HelloServlet extends HttpServlet {
 
         String name = request.getParameter("name");
 
-        if(name==null)
+        if(name == null)
             name = "Esther";
 
         message = message + name + "!";
@@ -71,12 +52,11 @@ public class HelloServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)throws IOException {
         String message = "";
 
         String lang = request.getParameter("lang");
-        if(lang==null)
+        if(lang == null)
             lang = "pt";
         switch(lang){
             case "pt":
@@ -95,10 +75,10 @@ public class HelloServlet extends HttpServlet {
 
         String name = request.getParameter("name");
 
-        if(name==null)
+        if(name == null)
             name = "Esther";
 
-        message = message+name+"!";
+        message = message + name + "!";
 
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
